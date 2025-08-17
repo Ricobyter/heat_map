9b007ea2eda6a2c0f56fe48b92bf985d78549e43
Dhruv <dhruvanandsingh2004@gmail.com>
2025-08-17 16:33:15 +0530

    added charts

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -4,10 +4,10 @@ import compass from "../assets/compass2.png";
 
 const HeatMap = ({ mapType, selectedLayer }) => {
   const mapSrcs = {
-    exposure_index: "/exposure_index.html",
-    vulnerability_index: "/vulnerability_index.html",
-    sensitivity_index: "/sensitivity_index.html",
-    adaptive_capacity_index: "/adaptive_capacity_index.html",
+    exposure_index: "/exposure_index_satellite.html",
+    vulnerability_index: "/vulnerability_index_satellite.html",
+    sensitivity_index: "/sensitivity_index_satellite.html",
+    adaptive_capacity_index: "/adaptive_capacity_satellite.html",
   };
 
   const mapName = {
@@ -30,7 +30,7 @@ const HeatMap = ({ mapType, selectedLayer }) => {
   if (selectedLayer === "Health Facilities") {
     src = "/exposure_index_health_facilities.html";
   } else {
-    src = mapSrcs[mapType] || "/vulnerability_index.html";
+    src = mapSrcs[mapType] || "/vulnerability_index_satellite.html";
   }
     const title =
     selectedLayer === "Health Facilities"
@@ -45,74 +45,6 @@ const HeatMap = ({ mapType, selectedLayer }) => {
           style={{ width: "100%", height: "100%", border: "none" }}
         ></iframe>
       </div>
-      {/* Legend */}
-
-      {/* <div className="flex justify-around items-center text-sm">
-        <div className="px-4 border-r-2 border-gray-900">
-          <div className="text-sm text-black">
-            <div className="font-semibold ">
-              Population Density (2025) / km^2:
-            </div>
-            <div className="grid grid-cols-2 gap-y-0.5 gap-x-6 text-xs">
-              {legendItems.map((item, index) => (
-                <div key={index} className="flex items-center space-x-1">
-                  <span
-                    className={`inline-block rounded-full bg-[#4B1E1E] ${item.size}`}
-                  ></span>
-                  <span>{item.density}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center space-y-4  ">
-          <div>
-            <h1 className="text-lg font-semibold ">{title}</h1>
-          </div>
-
-          <div className="flex flex-row text-xs space-x-2 px-2 ">
-            <div className="flex flex-col items-center space-y-1 ">
-              <span className="font-medium">High</span>
-              <div
-                className={`w-14 h-6 rounded-sm ${
-                  mapType === "adaptive_capacity_index"
-                    ? "bg-[#a1d885]"
-                    : "bg-[#ed0900]"
-                }`}
-              ></div>
-            </div>
-            <div className="flex flex-col items-center space-y-1">
-              <span className="font-medium">Medium</span>
-              <div
-                className={`w-14 h-6 rounded ${
-                  mapType === "adaptive_capacity_index"
-                    ? "bg-[#d6f5be]"
-                    : "bg-[#f5a319]"
-                }`}
-              ></div>
-            </div>
-            <div className="flex flex-col items-center space-y-1">
-              <span className="font-medium">Low</span>
-              <div
-                className={`w-14 h-6 rounded shadow-sm ${
-                  mapType === "adaptive_capacity_index"
-                    ? "bg-[#b7c6b0]"
-                    : mapType === "vulnerability_index"
-                    ? "bg-[#f2f3d0]"
-                    : "bg-[#ecc1aa]"
-                }`}
-              ></div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center ">
-          <div className="pl-2  border-l-2 border-gray-900">
-            <img src={compass} alt="" className="h-28" />
-          </div>
-        </div>
-      </div> */}
 
           <div className="bg-white px-6 py-4 rounded-md">
       {/* Main Header Container */}
