d91e30be11a7597ad13d2936a7c7cf6a01806039
Dhruv <dhruvanandsingh2004@gmail.com>
2025-08-26 23:16:42 +0530

    added some small changes

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header1 from '../components/Header1'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header1 />
+      
+    </div>
+  )
+}
+
+export default Homepage
