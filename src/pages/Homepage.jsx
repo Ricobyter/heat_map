901adab39e19bf28d389fb4bf8c76872816409f8
Dhruv <dhruvanandsingh2004@gmail.com>
2025-09-01 09:06:53 +0530

    changed districts to blocks

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header1 from '../components/Header1'
+import Hero from '../components/mainpage/Hero'
+import AboutHap from '../components/mainpage/AboutHap'
 
 const Homepage = () => {
   return (
     <div>
       <Header1 />
+      <Hero />
+      <AboutHap />
       
     </div>
   )
