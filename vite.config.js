4d30d519f0f0044ae367d9eadba4af6d81850b31
Dhruv <dhruvanandsingh2004@gmail.com>
2025-09-07 16:15:24 +0530

    edited  maps

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,27 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    // Custom plugin to add iframe headers
+    {
+      name: 'iframe-headers',
+      configureServer: (server) => {
+        server.middlewares.use((req, res, next) => {
+          // Remove X-Frame-Options to allow iframe embedding
+          res.removeHeader('X-Frame-Options');
+          // Allow all origins to embed this in iframe
+          res.setHeader('Content-Security-Policy', "frame-ancestors *;");
+          next();
+        });
+      },
+    }
   ],
+  server: {
+    headers: {
+      // Remove iframe restrictions for development
+      'Content-Security-Policy': "frame-ancestors *;"
+    }
+  }
 })
