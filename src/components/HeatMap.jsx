dd7f49d98c641f59a1b492029de2222159d59e4a
Dhruv <dhruvanandsingh2004@gmail.com>
2025-10-03 00:00:59 +0530

    changed color of i button

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -352,7 +352,7 @@ export default function HeatMap({
                 <div className="flex items-start gap-4">
                   <div className="bg-[#FFFCE8] border-2 border-[#FFEF10] rounded-lg p-3 flex items-center gap-3">
                     <div className="flex items-center justify-center w-8 h-8 bg-[#FFFCE8] rounded">
-                      <svg className="w-5 h-5 text-yellow-800" fill="currentColor" viewBox="0 0 20 20">
+                      <svg className="w-5 h-5 text-red-600" fill="currentColor" viewBox="0 0 20 20">
                         <path
                           fillRule="evenodd"
                           d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
